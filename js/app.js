30e2b2ea55fe6b063787194a6e50b2d579cd23f6
Ruy Machado <ruynetomachado@gmail.com>
2024-08-16 03:01:56 -0300

    Criação função reiniciar

--- js/app.js
+++ js/app.js
@@ -42,5 +42,7 @@ function embaralha(lista) {
 }
 
 function reiniciar(){
-
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
